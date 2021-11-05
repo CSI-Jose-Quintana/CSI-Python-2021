5176ab3774fa03304a3bfe1cc46258bc804a0e8a
CSI-Jose-Quintana <jose.quintana@sanignacio.pr>
2021-11-05 13:47:03 -0400

    ExperimentalData

--- Projects/Projectile-Motion/Jose-Quintana-ProjectileMotion.py
+++ Projects/Projectile-Motion/Jose-Quintana-ProjectileMotion.py
@@ -1,6 +1,5 @@
 import math
-
-
+from ExperimentalData import ExperimentalData
 # gun = "ADAR 2-15"
 # caliber = "5.56 X 45 mm"
 # ammunition = "M995"
@@ -11,20 +10,31 @@ import math
 
 #Convert your variables into parameters
 
-def ProjectileFunction(gun:str, caliber:str, ammunition: str, velocity_ms: int, Building:str, BuildingHeight:int, gravity:int):
+def ProjectileFunction(experimentalData: ExperimentalData):
+    time_s =math.sqrt((2 * experimentalData.BuildingHeight) / experimentalData.gravity_Ms)
 
-    time_s =math.sqrt((2 * BuildingHeight) / gravity_MS)
+    distance_m = (experimentalData.velocity_ms * time_s)
+    # distance_m = (experimentalData[velocity_ms] * time_s)
 
-    distance_m = (velocity_ms * time_s)
 
-    print(f"The gun selected for the experiment is {gun}. The caliber of {gun} is {caliber}.")
+    print(f"The gun selected for the experiment is {experimentalData.gun}. The caliber of {gun} is {caliber}.")
 
 #Convert your script parameters into a single JSON Object
+# experimentalData = {
 
+# "gun": "ADAR 2-15",
+# "caliber": "5.56 X 45 mm",
+# "ammunition": "M995",
+# "velocity_ms": 1013,
+# "Building":  "Caribbean Sea View",
+# "BuildingHeight": 334,
+# "gravity_Ms":  9.81
 
-ProjectileFunction("ADAR 2-15", "5.56 X 45 mm", "M995", 1013, "Caribbean Sea View", 334, 9.81)
+# }
 
+experimentalData = ExperimentalData("ADAR 2-15", "5.56 X 45 mm", "M995", 1013, "Caribbean Sea View", 334, 9.81)
 
+ProjectileFunction
 
 
 
