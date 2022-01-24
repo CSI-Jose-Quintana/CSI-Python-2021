9151e149b5a3bed1fa7e30511b0670030a9013c2
CSI-Jose-Quintana <jose.quintana@sanignacio.pr>
2022-01-24 10:45:28 -0400

    Update Jose-Quintana-Hangman1.py

--- Projects/Jose-Quintana-Hangman1.py
+++ Projects/Jose-Quintana-Hangman1.py
@@ -1,6 +1,31 @@
 import random
 word_list = ["buchipluma", "guasibiri", "glopeta", "acicalao", "masucamba", "sandungueo", "pichaera", "feca", "blinblineo", "banshee" ]
 
+def get_word(word_list):
+    word = random.choice(word_list)
+    return word.upper()
+
+def play(word):
+    word_completion = "_" * len(word)
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    tries = 6
+    print("Let's play Hand Hangman")
+    print("Theme: Reggaeton words")
+    print(display_hangman(tries))
+    print(word_completion)
+    print("\n")
+    while not guessed and tries > 0:
+        guess = input("Guess a letter or word").upper()
+        if len(guess) == 1 and guess.isalpha():
+            if guess in guessed_letters:
+                print("You already tried", guess, "!")
+            elif guess not in word:
+                print("Isn't in the word :(")
+                tries -= 1
+                guessed_letters.append(guess)
+
 def display_hangman(tries):
     stages = [ """
                     --------
@@ -11,7 +36,7 @@ def display_hangman(tries):
                     |   / \\
                     -
                     """,
-                    """"
+                    """
                     --------
                     |    |
                     |    O
@@ -20,12 +45,48 @@ def display_hangman(tries):
                     |   / 
                     -
                     """,
-                    """"
+                    """
                     --------
                     |    |
                     |    O
                     |   /|\\
                     |    |
+                    |   
+                    -
+                    """,
+                    """
+                    --------
+                    |    |
+                    |    O
+                    |   /|
+                    |    |
+                    |   
+                    -
+                    """,
+                    """
+                    --------
+                    |    |
+                    |    O
+                    |    |
+                    |    |
+                    |   
+                    -
+                    """,
+                     """"
+                    --------
+                    |    |
+                    |    O
+                    |   
+                    |    
+                    |   
+                    -
+                    """,
+                    """
+                    --------
+                    |    |
+                    |    
+                    | 
+                    |    
                     |   
                     -
                     """,
