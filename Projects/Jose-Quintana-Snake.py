bed4d0fd7c0573542827f33ef5e927368029b679
CSI-Jose-Quintana <jose.quintana@sanignacio.pr>
2022-02-28 15:15:05 -0400

    Update Jose-Quintana-Snake.py

--- Projects/Jose-Quintana-Snake.py
+++ Projects/Jose-Quintana-Snake.py
@@ -10,19 +10,25 @@ black = (0, 0, 0) #background color
 blue = (91, 57, 250) #snake
 red = (250, 76, 70) #game over
 
-dis_width = 400
-dis_height = 300
-dis=pygame.display.set_mode((dis_width, dis_height))
+dis_width = 600
+dis_height = 400
 
+dis=pygame.display.set_mode((dis_width, dis_height))
 pygame.display.set_caption("Snake game by JD")
 game_over=False
 
 snake_block = 10
-snake_speed = 30
+snake_speed = 15
 
 clock = pygame.time.Clock()
 
 font_style = pygame.font.SysFont(None, 25)
+score_font = pygame.font.SysFont(None, 25)
+
+def our_snake(snake_block, snake_list):
+    for x in snake_list:
+        pygame.draw.rect(dis, blue, [x[0], x[1], snake_block, snake_block])
+
 
 def message(msg, color):
     mesg = font_style.render(msg, True, color)
@@ -38,8 +44,11 @@ def gameRestart():
     x1_change = 0
     y1_change = 0
 
-    foodx = round(random.randrange(0, dis_width - snake_block) / 10) *10
-    foody = round(random.randrange(0, dis_width - snake_block) / 10) *10
+    snake_List=[]
+    length_of_snake = 1
+
+    foodx = round(random.randrange(0, dis_width - snake_block) / 10) * 10
+    foody = round(random.randrange(0, dis_height - snake_block) / 10) * 10
 
 
     while not game_over:
@@ -80,8 +89,26 @@ def gameRestart():
         y1 += y1_change
         dis.fill(black)  
         pygame.draw.rect(dis, green, [foodx, foody, snake_block, snake_block])  #crea el food      
-        pygame.draw.rect(dis, blue, [x1, y1, snake_block, snake_block]) # crea el snake y lo ubica en la pantalla
+        # pygame.draw.rect(dis, blue, [x1, y1, snake_block, snake_block]) # crea el snake y lo ubica en la pantalla
+        snake_Head = []
+        snake_Head.append(x1)
+        snake_Head.append(y1)
+        snake_List.append(snake_Head)
+        if len(snake_List) > length_of_snake:
+            del snake_List[0]
+
+        for x in snake_List[: -1]:
+            if x == snake_Head:
+                game_close = True
+        
+        our_snake(snake_block, snake_List)
+
         pygame.display.update()   
+
+        if x1 == foodx and y1 == foody:
+            foodx = round(random.randrange(0, dis_width - snake_block) / 10) * 10
+            foody = round(random.randrange(0, dis_height - snake_block) / 10) * 10
+            length_of_snake += 1
 
         clock.tick(snake_speed) 
 
