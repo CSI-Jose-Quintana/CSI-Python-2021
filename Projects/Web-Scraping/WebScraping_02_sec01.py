9dd9a57a4611b2cc867f027624bfb3246196b72b
CSI-Jose-Quintana <jose.quintana@sanignacio.pr>
2022-04-04 10:23:44 -0400

    hhh

---
+++ Projects/Web-Scraping/WebScraping_02_sec01.py
@@ -0,0 +1,10 @@
+import requests
+import bs4
+res = requests.get('https://www.sanignacio.pr')
+res.raise_for_status()
+SanIgnaciopr = bs4.BeautifulSoup(res.text, 'html.parser')
+elems = SanIgnaciopr.select('title')
+print(type(SanIgnaciopr))
+print(len(elems))
+print(str(elems))
+print(elems[0].getText())
